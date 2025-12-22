80d43a8337448f605689eedf295bb70765451b65
Ofek Levy <ofeklev@post.bgu.ac.il>
2025-12-22 23:00:34 +0200

    saving work for SharedVector

--- src/main/java/memory/SharedVector.java
+++ src/main/java/memory/SharedVector.java
@@ -9,46 +9,121 @@ public class SharedVector {
     private ReadWriteLock lock = new java.util.concurrent.locks.ReentrantReadWriteLock();
 
     public SharedVector(double[] vector, VectorOrientation orientation) {
-        // TODO: store vector data and its orientation
+        // store vector data and its orientation
+        if (vector == null || orientation == null)
+            throw new IllegalArgumentException("vector and orientation cannot be null");
+        this.vector = vector;
+        this.orientation = orientation;
     }
 
     public double get(int index) {
-        // TODO: return element at index (read-locked)
-        return 0;
+        // return element at index (read-locked)
+        readLock();
+        try {
+            if (index < 0 || index >= vector.length)
+                throw new IndexOutOfBoundsException("Index: " + index + ", out of bounds for length " + vector.length);
+            return vector[index];
+        }
+        finally {
+            readUnlock();
+        }
+
     }
 
     public int length() {
-        // TODO: return vector length
-        return 0;
+        // return vector length
+        return vector.length;
     }
 
     public VectorOrientation getOrientation() {
-        // TODO: return vector orientation
-        return null;
+        // return vector orientation
+        readLock();
+        try {
+            return orientation;
+        }
+        finally {
+            readUnlock();
+        }
     }
 
     public void writeLock() {
-        // TODO: acquire write lock
+        // acquire write lock
+        lock.writeLock().lock();
     }
 
     public void writeUnlock() {
-        // TODO: release write lock
+        // release write lock
+        lock.writeLock().unlock();
     }
 
     public void readLock() {
-        // TODO: acquire read lock
+        // acquire read lock
+        lock.readLock().lock();
     }
 
     public void readUnlock() {
-        // TODO: release read lock
+        // release read lock
+        lock.readLock().unlock();
     }
 
     public void transpose() {
-        // TODO: transpose vector
+        // transpose vector
+        writeLock();
+        try {
+            if (orientation == VectorOrientation.ROW_MAJOR) {
+                orientation = VectorOrientation.COLUMN_MAJOR;
+            } else {
+                orientation = VectorOrientation.ROW_MAJOR;
+            }
+        } finally {
+            writeUnlock();
+        }
     }
 
     public void add(SharedVector other) {
-        // TODO: add two vectors
+        // add two vectors
+        if (other == null)
+            throw new IllegalArgumentException("other cannot be null");
+
+        // case this = other
+        if (other == this) {
+            writeLock();
+            try {
+                for (int i = 0; i < vector.length; i++)
+                    vector[i] += vector[i];
+            }
+            finally {
+                writeUnlock();
+            }
+            return;
+        }
+
+        // copy other with readLock to prevent deadlock
+        final double[] otherCopy;
+        final VectorOrientation otherOrientation;
+        other.readLock();
+        try {
+            if (this.vector.length != other.vector.length)
+                throw new IllegalArgumentException("vector and other vector lengths do not match");
+            otherCopy = other.vector.clone();
+            otherOrientation = other.orientation;
+        }
+        finally {
+            other.readUnlock();
+        }
+
+        // update this with writeLock
+        this.writeLock();
+        try {
+            if (this.orientation != otherOrientation)
+                throw new IllegalArgumentException("orientations do not match");
+            for (int i = 0; i < vector.length; i++)
+                vector[i] += otherCopy[i];
+        }
+        finally {
+            this.writeUnlock();
+        }
+
     }
 
     public void negate() {
